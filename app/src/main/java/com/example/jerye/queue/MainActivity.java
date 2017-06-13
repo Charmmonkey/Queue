ea3902856b27eca39de2d7c4ef83419bf9afc85e
Kevin Hsu <jeryen.hsu@gmail.com>
2017-06-12 22:17:42 -0700

    1.0.1 functional firebase realtime db push and retrieve

--- app/src/main/java/com/example/jerye/queue/MainActivity.java
+++ app/src/main/java/com/example/jerye/queue/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.jerye.queue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditText;
+    private Button mButton;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private String mUsername;
+    private ChildEventListener mChildEventListener;
+    private RecyclerView mRecyclerView;
+    private MessageAdapter mMessageAdapter;
+    private List<Message> messageList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mEditText = (EditText) findViewById(R.id.edit_textbox);
+        mButton = (Button) findViewById(R.id.send_button);
+        mRecyclerView = (RecyclerView) findViewById(R.id.text);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        messageList = new ArrayList<>();
+        mMessageAdapter = new MessageAdapter(this, messageList);
+        mRecyclerView.setAdapter(mMessageAdapter);
+        mUsername = "Kevin";
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message message = new Message(mEditText.getText().toString(), mUsername, null);
+
+                mDatabaseReference.push().setValue(message);
+                mEditText.setText("");
+            }
+        });
+
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Message message = dataSnapshot.getValue(Message.class);
+                mMessageAdapter.add(message);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildEventListener);
     }
+
 }
---
+++ app/src/main/java/com/example/jerye/queue/MessageAdapter.java
@@ -0,0 +1,73 @@
+package com.example.jerye.queue;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by jerye on 6/12/2017.
+ */
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    public Context mContext;
+    public List<Message> mMessageList;
+
+    public MessageAdapter(Context context, List<Message> list) {
+        mContext = context;
+        mMessageList = list;
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        holder.messageName.setText(mMessageList.get(position).getName());
+        holder.messageContent.setText(mMessageList.get(position).getText());
+
+    }
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.message_layout, parent, false);
+
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMessageList == null) {
+            return 0;
+        } else {
+            return mMessageList.size();
+        }
+    }
+
+    public void add(Message message) {
+        mMessageList.add(message);
+        notifyDataSetChanged();
+    }
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageName;
+        TextView messageContent;
+
+        public MessageViewHolder(View view) {
+            super(view);
+
+            messageName = (TextView) view.findViewById(R.id.message_name);
+            messageContent = (TextView) view.findViewById(R.id.message_content);
+        }
+
+        ;
+    }
+}
