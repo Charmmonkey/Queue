0fb4cbe22446d46a73480734302ce1cd15bdec9c
Kevin Hsu <jeryen.hsu@gmail.com>
2017-07-07 00:39:46 -0700

    1.1.7 successful cloud function test

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,48 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+  // // Create and Deploy Your First Cloud Functions
+  // // https://firebase.google.com/docs/functions/write-firebase-functions
+  //
+  // exports.helloWorld = functions.https.onRequest((request, response) => {
+  //  response.send("Hello from Firebase!");
+  // });
+
+  const admin = require('firebase-admin');
+  admin.initializeApp(functions.config().firebase);
+
+//  exports.sendNotification = functions.database.ref('').onWrite(event => {
+//      const roomId = event.params.roomId;
+//      const trackId = event.params.trackId;
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//      console.log(userName,' added', songAdded, ' to the playlist!');
+//
+//      const getSongAddedPromise = admin.database().ref('/${roomId}/tracks/${trackId}/name').once('child_added');
+//      const getSongAddedPromise = admin.database().ref('/${roomId}/tracks/${trackId}/author').once('child_added');
+//
+//
+//      return Promise.all([getSongAddedPromise]).then(results => {
+//          const songAdded = results[0];
+//
+//          // Notification Details
+//          const payload = {
+//              notification: {
+//                  title: '',
+//                  body: '',
+//                  icon: ''
+//              }
+//          }
+//      })
+//
+//  })
 
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
-
-//exports.sendNotification = functions.database.ref('')
+exports.makeUppercase = functions.database.ref('/{pushId}/messages/{messageId}')
+    .onWrite(event => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = event.data.child("text").val();
+      console.log('Uppercasing', event.params.messageId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return event.data.ref.parent.child('uppercase').set(uppercase);
+    });
